4416ad70ff3fffa741b5eec7a5a847106437cce5
DSiminski <siminskidayane@gmail.com>
2022-06-23 21:43:59 -0300

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,65 @@
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+<div class = "header is fullhd">
+<h1>Header</h1>
+</div>
+
+ <div class = "container">
+    <div class = "container is-widescreen">
+        <div class="columns is-mobile ">
+    <div class="column is-three-fifths is-offset-one-fifth">
+      Home
+    </div>
+  </div>
+    <div class="columns is-mobile">
+      <div class="column is-three-fifths is-offset-one-fifth">
+        Agendamento
+      </div>
+    </div>
+    <div class="columns is-mobile">
+      <div class="column is-three-fifths is-offset-one-fifth">
+        Médicos
+      </div>
+    </div>
+    <div class="columns is-mobile">
+      <div class="column is-three-fifths is-offset-one-fifth">
+        Pacientes
+      </div>
+    </div>
+    </div>
   <router-view/>
+      </div>
+
+  <footer class="footer">
+    <div>
+      
+    </div>
+  </footer>
 </template>
 
 <style lang="scss">
 #app {
+
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #3b1097;
 }
 
 nav {
-  padding: 30px;
+  padding: 150px;
 
   a {
     font-weight: bold;
-    color: #2c3e50;
-
+    
     &.router-link-exact-active {
-      color: #42b983;
+      
     }
   }
+}
+.header{
+
+text-align: center;
+color: black;
 }
 </style>
