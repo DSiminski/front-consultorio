6e5fa5731706eb446909f15d1147852f8e0718cd
DSiminski <siminskidayane@gmail.com>
2022-06-29 03:44:53 -0300

    SideBar

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,48 @@
 
 <template>
-<div class="content">
-  <div class = "column is fullhd header">
-    <h1>Header</h1>
+  <div class="content">
+    <div class="column is-fullhd header">
+      <h1>Header</h1>
+    </div>
+
+    <div class="columns mt-3">
+      <div class="column is-3">
+        <aside class="menu">
+          <p class="menu-label has-text-left pl-3">
+            Home
+          </p>
+              <p class="menu-label has-text-left pl-3"> Administração </p>
+               <ul class="menu-list">
+                  <li><a>Convenio</a></li>
+                  <li><a>Especialidade</a></li>
+                </ul>
+              <p class="menu-label has-text-left pl-5"> Úsuario </p>
+               <ul class="menu-list">
+                  <li><a>Médicos</a></li>
+                  <li><a>Secretárias</a></li>
+                  <li><a>Paciente</a></li>
+                </ul>
+              <p class="menu-label has-text-left pl-5"> Agenda </p>
+               <ul class="menu-list">
+                  <li><a>Agendamentos</a></li>
+                  <li><a>Histórico</a></li>
+                </ul>
+        </aside>
+      </div>
+      <div class="column is-9">
+        <router-view >
+        </router-view >
+      </div>
+    </div>
+  <Footer>
+      <div class="column is-fullhd footer">
+        <h1>Footer</h1>
+      </div>
+</Footer>
   </div>
 
-  <div class = "columns mt-3">
-        <div class="column is-3">
-              <aside class="menu">
-                <p class="menu-label">
-                   Home
-                  </p>
-                  <ul class="menu-list">
-                    <li><a>Paciente</a></li>
-                    <li><a>Médico</a></li>
-                    <li><a>Secretária</a></li>
-                  </ul>
-              </aside>
-          </div>
-  <div class="column is-9">
-      <router-view/><router-view/>
-    </div>
-  </div>
-</div>
-<Footer>
-  <div class="column is-12 has footer">
-    <h1>Footer</h1>
-  </div>
-</Footer>
 </template>
 
-
-<!-- <script> 
-  import { HomeView } from "@/views/HomeView.vue";
-  
-  export default {
-  name: 'App',
-  components: {
-    HomeView
-  }
-}
-</script> -->
 
 <style lang="scss">
 @import "~bulma/bulma.sass";
@@ -50,33 +52,44 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align:center;
+  text-align: center;
   color: black;
-  
+  height: 100vh;
+
 }
-.header{
-background-color: aqua;
-max-height: 80px;
-text-align: center;
-  h1{color: black;}
+.content {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 100vh;
+}
+
+.header {
+  background-color: aqua;
+  max-height: 80px;
+  text-align: center;
+
+  h1 {
+    color: black;
+  }
 }
 
 .footer {
   background-color: aqua;
   text-align: center;
-  max-height: 80px;
-  justify-content: flex-end;
-  align-items:end;
-h1{
-      color: beige;
-    }
+  h1 {
+    color: black;
+  }
 }
-.menu{
+
+.menu {
   color: aqua;
+  background-color:palevioletred;
+  text-align: left;
 }
 
 nav {
-a {
+  a {
     font-weight: bold;
     color: brown;
 
@@ -86,5 +99,4 @@ a {
   }
 
 }
-
 </style>
